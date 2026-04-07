3a59e270d02645105d5df3d14a77d90dfec677f1
sarvesh973 <sarveshhigh1254@gmail.com>
2026-04-07 17:43:41 +0530

    auto: update code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.vedastro_ai"
+    namespace = "com.vedastro.ai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.vedastro_ai"
+        applicationId = "com.vedastro.ai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
